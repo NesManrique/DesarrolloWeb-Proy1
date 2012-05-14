7569876306cb190de61763b324a56248307a0fa8
Samuel Bártoli <smbartoli@gmail.com>
2012-05-14 16:33:19 -0430

    actualizado estructura. eliminada carpeta sesion

--- BST/BST/settings.py
+++ BST/BST/settings.py
@@ -119,7 +119,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'BST.sesion',
+    'user_manager',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
--- BST/BST/urls.py
+++ BST/BST/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.conf.urls.defaults import *
-from sesion.views import *
+from user_manager.views import *
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
